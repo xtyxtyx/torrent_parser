6e1bfd689e60d1c00e513957741f05512d38c1a6
xuty <xty50337@hotmail.com>
2019-07-18 20:19:43 +0800

    add tests

--- test/torrent_parser_test.dart
+++ test/torrent_parser_test.dart
@@ -48,63 +48,6 @@ void main() {
 
   });
 
-  group('reader tests', () {
-    DataReader reader;
-
-    setUp(() {
-      final data = 'hello world'.runes.toList();
-      reader = DataReader(data);
-    });
-
-    test('takeString', () {
-      expect(reader.peekString(5), equals('hello'));
-      expect(reader.peekString(5), equals('hello'));
-      expect(reader.peekString(1), equals('h'));
-      expect(reader.takeString(5), equals('hello'));
-      expect(reader.takeString(), equals(' '));
-      expect(reader.takeString(5), equals('world'));
-      expect(reader.takeString(), equals(null));
-      expect(reader.takeString(5), equals(null));
-    });
-
-    test('match', () {
-      expect(reader.match('hello'), equals('hello'));
-      expect(reader.match('hello'), equals('hello'));
-      expect(reader.match('hola'), equals(null));
-    });
-
-    test('matches', () {
-      expect(reader.matches('hello'), equals(true));
-      expect(reader.matches('hello'), equals(true));
-      expect(reader.matches('hola'), equals(false));
-    });
-
-    test('readInt', () {
-      final reader = DataReader.fromString(('123abc456def-789'));
-      expect(reader.readInt(), equals(123));
-      expect(reader.expect('abc'), anything);
-      expect(reader.readInt(), equals(456));
-      expect(reader.expect('def'), anything);
-      expect(reader.readInt(), equals(-789));
-      expect(reader.atEOF, equals(true));
-    });
-    
-
-    test('atEOf', () {
-      expect(reader.atEOF, equals(false));
-      expect(reader.takeString(11), equals('hello world'));
-      expect(reader.atEOF, equals(true));
-    });
-
-    test('expect', () {
-      expect(() => reader.expect('hola'), throwsA(equals('hola expected at 0')));
-      expect(reader.expect('hello'), equals('hello'));
-      expect(reader.expect(' '), equals(' '));
-      expect(reader.expect('world'), equals('world'));
-      expect(() => reader.expect('sekai'), throwsA(equals('sekai expected at 11')));
-    });
-  });
-
   group('torrent data tests', () {
 
     // test('toString', () {
@@ -120,12 +63,6 @@ void main() {
 
   group('TorrentInfo tests', () {
 
-    test('_splitPieces', () {
-      expect(TorrentInfo.splitPieces('b667507d95b114bfd2866a6863e3ad95a89e52a5ecf6ee1e13eede3a7133b8f3571860973f49b94b'), equals([
-        'b667507d95b114bfd286', '6a6863e3ad95a89e52a5',
-        'ecf6ee1e13eede3a7133', 'b8f3571860973f49b94b'
-      ]));
-    });
 
   });
 }
